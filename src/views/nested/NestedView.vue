99510e9c50d16dfbe25eb2574f82c8e6f62f2f60
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-06-29 23:04:40 +0900

    [7] : 404 Error, Nested Routes

---
+++ src/views/nested/NestedView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <RouterLink class="nav-link" active-class="active" to="/nested/one">
+          Nested One
+        </RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" active-class="active" to="/nested/two">
+          Nested Two
+        </RouterLink>
+      </li>
+    </ul>
+    <hr />
+    <!-- Nested Routes -->
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
