85b3ab5f2cea7faaf815ede38af9d116b02103ce
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-02 22:57:09 +0900

    [13] : 공통 컴포넌트 분리

---
+++ src/components/posts/PostFilter.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent>
+    <div class="row g-3">
+      <!-- Search / Filter -->
+      <div class="col">
+        <input
+          :value="title"
+          @input="$emit('update:title', event.target.value)"
+          type="text"
+          class="form-control"
+          placeholder="제목으로 검색해 주세요."
+        />
+      </div>
+      <!-- Change page limit -->
+      <div class="col-3">
+        <select
+          :value="limit"
+          @input="$emit('update:limit', event.target.value)"
+          class="form-select"
+        >
+          <option value="3">3개씩 보기</option>
+          <option value="6">6개씩 보기</option>
+          <option value="9">9개씩 보기</option>
+        </select>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    limit: Number,
+  },
+});
+
+/** 다중 v-model */
+defineEmits(['update:title', 'update:limit']);
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <template>
   <div>Home View 🏡</div>
   <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+  <hr class="my-4" />
+  <AppGrid :items="items" v-slot="{ item }">
+    <AppCard>
+      {{ item }}
+    </AppCard>
+  </AppGrid>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import AppCard from '@/components/AppCard.vue';
+import AppGrid from '@/components/AppGrid.vue';
 
 const router = useRouter();
 const goAboutPage = () => {
@@ -12,6 +21,8 @@ const goAboutPage = () => {
     name: 'About',
   });
 };
+
+const items = ref(['A', 'B', 'C', 'D']);
 </script>
 
 <style></style>
