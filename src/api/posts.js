31552fa614e4afc77f0efd7aea6deff4bf22433f
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-02 13:57:29 +0900

    [10] : Pagination, Filter 구현

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 /** 게시글 목록 조회 - 이 함수를 호출하면 posts 배열의 모든 게시물 목록을 얻을 수 있다. */
-export function getPosts() {
-  return axios.get('http://localhost:5000/posts');
+export function getPosts(params) {
+  return axios.get('http://localhost:5000/posts', { params });
 }
 
 /** 배열에 있는 데이터를 id로 조회 - item.id와 일치하는 id를 찾는다. */
